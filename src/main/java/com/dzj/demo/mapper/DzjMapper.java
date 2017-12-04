fa5fdef17b817d2b8efae37d221ca9f3d94da562
hongbo.gao <hongbo.gao@rakuten.com>
2017-12-04 18:26:04 +0900

    add HSQLDB support

--- src/main/java/com/dzj/demo/mapper/DzjMapper.java
+++ src/main/java/com/dzj/demo/mapper/DzjMapper.java
@@ -8,8 +8,6 @@ import java.util.List;
 */
 public interface DzjMapper {
 
-    //生成代码开始 don't delete
-
     int insertdzj(DzjBean object);
 
     int updatedzj(DzjBean object);
@@ -17,5 +15,5 @@ public interface DzjMapper {
     List<DzjBean> querydzj(DzjBean object);
 
     DzjBean querydzjLimit1(DzjBean object);
-    //生成代码结束 don't delete
+
 }
