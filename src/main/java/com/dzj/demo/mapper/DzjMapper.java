2351c1761dc0251dd8a12d51fb2e0ca25d326f51
hongbo.gao <hongbo.gao@rakuten.com>
2017-12-05 17:18:23 +0900

    update sql

--- src/main/java/com/dzj/demo/mapper/DzjMapper.java
+++ src/main/java/com/dzj/demo/mapper/DzjMapper.java
@@ -1,6 +1,9 @@
 package com.dzj.demo.mapper;
 
 import com.dzj.demo.domain.DzjBean;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 /**
@@ -8,12 +11,16 @@ import java.util.List;
 */
 public interface DzjMapper {
 
+    @Insert("insert into dzj_txt_mt(dzjText, dzjTitle) values(#{dzjText}, #{dzjTitle})")
     int insertdzj(DzjBean object);
 
+    @Update("update dzj_txt_mt set dzjText = #{dzjText}, dzjTitle = #{dzjTitle} where Id=#{Id}")
     int updatedzj(DzjBean object);
 
+    @Select("select * from dzj_txt_mt")
     List<DzjBean> querydzj(DzjBean object);
 
+    @Select("select * from dzj_txt_mt where Id = #{Id}")
     DzjBean querydzjLimit1(DzjBean object);
 
 }
