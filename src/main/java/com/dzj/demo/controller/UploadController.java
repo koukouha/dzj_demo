fb91d9af0d621128c027d984adc354124f7d53bc
hongbo.gao <hongbo.gao@rakuten.com>
2017-12-06 12:13:40 +0900

    update controller

--- src/main/java/com/dzj/demo/controller/UploadController.java
+++ src/main/java/com/dzj/demo/controller/UploadController.java
@@ -1,11 +1,18 @@
 package com.dzj.demo.controller;
 
+import com.dzj.demo.domain.DzjBean;
+import com.dzj.demo.mapper.DzjMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by hongbo.gao on 2017/12/5.
@@ -13,8 +20,33 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class UploadController {
 
-	@RequestMapping(value = "upload.do", method = RequestMethod.POST)
-	public ModelAndView upload(@RequestParam(value="file",required=false)MultipartFile file){
+	@Autowired
+	DzjMapper dzjMapper;
 
+	@RequestMapping(value = "/upload.do", method = RequestMethod.POST)
+	@ResponseBody
+	public String upload(@RequestParam(value="datafile",required=false)MultipartFile file){
+		try {
+			String text = convertInputStreamToString(file.getInputStream());
+			DzjBean dzj = new DzjBean();
+			dzj.setDzjTitle(file.getOriginalFilename());
+			dzj.setDzjText(text);
+			dzjMapper.insertdzj(dzj);
+			return "success";
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "fail";
+	}
+
+	static String convertInputStreamToString(InputStream is) throws IOException {
+		InputStreamReader reader = new InputStreamReader(is);
+		StringBuilder builder = new StringBuilder();
+		char[] buffer = new char[512];
+		int read;
+		while (0 <= (read = reader.read(buffer))) {
+			builder.append(buffer, 0, read);
+		}
+		return builder.toString();
 	}
 }
